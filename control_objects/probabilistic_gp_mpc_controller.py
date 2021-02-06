- Added dynamic visualizations - Added weight action in cost function - Corrected error in the computation of future variance state - Removed unecessary files - Cleaned some parts of the main.py file

control_objects/probabilistic_gp_mpc_controller.py
@@ -17,14 +17,18 @@ torch.set_default_tensor_type(torch.DoubleTensor)
 
 class ProbabiliticGpMpcController(BaseControllerObject):
 	def __init__(self, observation_space, action_space, params_controller, params_train, params_actions_optimizer,
-			params_constraints_states, hyperparameters_init, target_state, weights_matrix_cost_function,
-			weight_matrix_cost_function_terminal, constraints_hyperparams, env_to_control, folder_save,
-			num_repeat_actions):
+			params_constraints_states, hyperparameters_init,
+			target_state, weight_matrix_states_cost_function, weight_matrix_states_cost_function_terminal,
+			target_action, weight_matrix_actions_cost_function,
+			constraints_hyperparams, env_to_control, folder_save, num_repeat_actions):
 		BaseControllerObject.__init__(self, observation_space, action_space)
 		self.models = []
-		self.weight_matrix_cost_function = torch.Tensor(weights_matrix_cost_function)
-		self.weight_matrix_cost_function_terminal = torch.Tensor(weight_matrix_cost_function_terminal)
+		self.weight_matrix_cost_function = \
+			torch.block_diag(torch.Tensor(weight_matrix_states_cost_function), 
+				torch.Tensor(weight_matrix_actions_cost_function))
+		self.weight_matrix_cost_function_terminal = torch.Tensor(weight_matrix_states_cost_function_terminal)
 		self.target_state = torch.Tensor(target_state)
+		self.target_state_action = torch.cat((self.target_state, torch.Tensor(target_action)))
 		for observation_idx in range(observation_space.shape[0]):
 			self.models.append(ExactGPModelMonoTask(None, None, self.num_inputs))
 
@@ -128,15 +132,23 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 			self.p_train.start()
 			self.num_cores_main -= 1
 
-	def cost_fct_on_uncertain_inputs(self, m_state, s_state, m_action, s_action):
+	def cost_fct_on_uncertain_inputs(self, m_state, s_state, m_action):
 		'''This function computes the quadratic cost of a state distribution given the weight matrix, and target state.
 		m_state and s_state can be either the full trajectory (m_state.ndim=2 and s_state.ndim=3),
 		or a single state (m_state.ndim=1 and s_state.ndim=2)'''
-		error = m_state - self.target_state
-		mean_cost = torch.diagonal(torch.matmul(s_state, self.weight_matrix_cost_function), dim1=-1, dim2=-2).sum(-1) + \
-					torch.matmul(torch.matmul(error[..., None].transpose(-1, -2), self.weight_matrix_cost_function),
+		if s_state.ndim == 3:
+			error = torch.cat((m_state, m_action), 1) - self.target_state_action
+			s_state_action = torch.cat((
+				torch.cat((s_state, torch.zeros((s_state.shape[0], s_state.shape[1], m_action.shape[1]))), 2),
+				torch.zeros((s_state.shape[0], m_action.shape[1], s_state.shape[1] + m_action.shape[1]))), 1)
+		else:
+			error = torch.cat((m_state, m_action), 0) - self.target_state_action
+			s_state_action = torch.block_diag(s_state, torch.zeros((m_action.shape[0], m_action.shape[0])))
+		expected_cost = torch.diagonal(torch.matmul(s_state_action, self.weight_matrix_cost_function),
+				dim1=-1, dim2=-2).sum(-1) + \
+				torch.matmul(torch.matmul(error[..., None].transpose(-1, -2), self.weight_matrix_cost_function),
 						error[..., None]).squeeze()
-		TS = self.weight_matrix_cost_function @ s_state
+		TS = self.weight_matrix_cost_function @ s_state_action
 		s_cost_term_1 = torch.diagonal(2 * TS @ TS, dim1=-1, dim2=-2).sum(-1)
 		s_cost_term_2 = TS @ self.weight_matrix_cost_function
 		s_cost_term_3 = (4 * error[..., None].transpose(-1, -2) @ s_cost_term_2 @ error[..., None]).squeeze()
@@ -160,9 +172,9 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 				constraint_penalty_max = ((1 - distribution_state.cdf(
 					torch.Tensor(self.constraints_states["states_max"]))) * \
 									  self.constraints_states['area_penalty_multiplier']).diagonal(0, -1, -2).sum(-1)
-			mean_cost = mean_cost + constraint_penalty_max + constraint_penalty_min
+			expected_cost = expected_cost + constraint_penalty_max + constraint_penalty_min
 
-		return mean_cost, s_cost
+		return expected_cost, s_cost
 
 	def terminal_cost(self, m_state, s_state):
 		error = m_state - self.target_state
@@ -194,10 +206,10 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 			mu_states_pred[idx_time] = mu_states_pred[idx_time - 1] + d_state  # torch.clamp(, 0, 1)
 			s_states_pred[idx_time] = d_s_state + s_states_pred[idx_time - 1] + \
 									  s_inputs_pred[:s_states_pred.shape[1]] @ v + \
-									  v.t() @ s_inputs_pred[:, :s_states_pred.shape[1]]
+									  v.t() @ s_inputs_pred[:s_states_pred.shape[1]].t()
 
 		costs_trajectory, costs_trajectory_variance = self.cost_fct_on_uncertain_inputs(mu_states_pred[:-1],
-			s_states_pred[:-1], actions, torch.zeros_like(actions))
+			s_states_pred[:-1], actions)
 		cost_trajectory_final, costs_trajectory_variance_final = self.terminal_cost(mu_states_pred[-1],
 			s_states_pred[-1])
 		costs_trajectory = torch.cat((costs_trajectory, cost_trajectory_final[None]), 0)
@@ -219,19 +231,22 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 			self.predict_trajectory(actions_input, mu_observation, s_observation, iK, beta)
 		if self.clip_lower_bound_cost_to_0:
 			costs_trajectory_lcb = torch.clamp(costs_trajectory_lcb, 0, np.inf)
-		cost_trajectory = costs_trajectory_lcb.mean()
-		gradients_cost = torch.autograd.grad(cost_trajectory, actions, retain_graph=False)[0]
+		mean_cost_trajectory_lcb = costs_trajectory_lcb.mean()
+		gradients_cost = torch.autograd.grad(mean_cost_trajectory_lcb, actions, retain_graph=False)[0]
 
-		self.cost_trajectory_mean_lcb = cost_trajectory.detach()
+		self.cost_trajectory_mean_lcb = mean_cost_trajectory_lcb.detach()
 		self.mu_states_pred = mu_states_pred.detach()
 		self.costs_trajectory = costs_trajectory.detach()
 		self.s_states_pred = s_states_pred.detach()
 		self.costs_trajectory_variance = costs_trajectory_variance.detach()
 
-		return cost_trajectory.detach().numpy(), gradients_cost[:, 0].detach().numpy()
+		return mean_cost_trajectory_lcb.detach().numpy(), gradients_cost[:, 0].detach().numpy()
 
 	@staticmethod
 	def calculate_factorizations(x, y, models):
+		""" Function inspired from
+		https://github.com/nrontsis/PILCO/blob/6a962c8e4172f9e7f29ed6e373c4be2dd4b69cb7/pilco/models/mgpr.py#L81,
+		reimplemented from tensorflow to pytorch """
 		K = torch.stack([model.covar_module(x).evaluate() for model in models])
 		batched_eye = torch.eye(K.shape[1]).repeat(K.shape[0], 1, 1)
 		L = torch.cholesky(K + torch.stack([model.likelihood.noise for model in models])[:, None] * batched_eye)
@@ -241,6 +256,14 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 		return iK, beta
 
 	def predict_given_factorizations(self, m, s, iK, beta):
+		"""
+		 Approximate GP regression at noisy inputs via moment matching
+		 IN: mean (m) (row vector) and (s) variance of the state
+		 OUT: mean (M) (row vector), variance (S) of the action and inv(s)*input-ouputcovariance
+		Function inspired from
+		https://github.com/nrontsis/PILCO/blob/6a962c8e4172f9e7f29ed6e373c4be2dd4b69cb7/pilco/models/mgpr.py#L81,
+		reinterpreted from tensorflow to pytorch
+		 """
 		s = s[None, None, :, :].repeat([self.num_states, self.num_states, 1, 1])
 		inp = (self.x[self.indexes_memory_gp[:beta.shape[1]]] - m)[None, :, :].repeat([self.num_states, 1, 1])
 		lengthscales = torch.stack([model.covar_module.base_kernel.lengthscale[0] for model in self.models])
@@ -346,7 +369,7 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 			self.action_previous_iter = next_action
 			actions = torch.Tensor(actions)
 			cost, cost_var = self.cost_fct_on_uncertain_inputs(normed_mu_observation, normed_s_observation,
-				actions[0], 0)
+				actions[0])
 			denorm_act = next_action[0] * (self.action_space.high - self.action_space.low) + self.action_space.low
 			# denorm_states = self.mu_states_pred[1:] * \
 			# (self.observation_space.high - self.observation_space.low) + self.observation_space.low
@@ -358,9 +381,11 @@ class ProbabiliticGpMpcController(BaseControllerObject):
 				'predicted states std': std_states[1:],
 				'predicted actions': actions,
 				'cost': cost.item(), 'cost std': cost_var.sqrt().item(),
-				'mean cost trajectory': self.costs_trajectory.mean().item(),
-				'mean cost trajectory std': self.costs_trajectory_variance.sqrt().mean().item(),
-				'lower bound cost trajectory': self.cost_trajectory_mean_lcb.item()}
+				'predicted costs': self.costs_trajectory[1:],
+				'predicted costs std': self.costs_trajectory_variance[1:].sqrt(),
+				'mean predicted cost': np.min([self.costs_trajectory[1:].mean().item(), 3]),
+				'mean predicted cost std': self.costs_trajectory_variance[1:].sqrt().mean().item(),
+				'lower bound mean predicted cost': self.cost_trajectory_mean_lcb.item()}
 			for key in add_info_dict.keys():
 				if not key in self.prediction_info_over_time:
 					self.prediction_info_over_time[key] = [add_info_dict[key]]

control_objects/utils.py
@@ -4,6 +4,7 @@ import os
 import torch
 import gpytorch
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -14,80 +15,231 @@ from sklearn.linear_model import LinearRegression, MultiTaskLasso
 
 from .gp_models import ExactGPModelMonoTask
 
+matplotlib.rc('font', size='6')
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+ALPHA_CONFIDENCE_BOUNDS = 0.3
+
+
+def draw_history(states, actions, states_next, prediction_info_over_time, num_repeat_actions=1,
+		constraints_states=None, num_iter_prediction_ahead_show=3, fig=None, axes=None,
+		update_x_limit=True):
+	indexes = np.arange(0, len(states) * num_repeat_actions, num_repeat_actions)
+	indexes_control = np.array(prediction_info_over_time['iteration']) // num_repeat_actions
+	predictions_over_time_show = np.zeros_like(states_next)
+	predictions_over_time_show[indexes_control] = \
+		[prediction[num_iter_prediction_ahead_show - 1].numpy()
+			for prediction in prediction_info_over_time['predicted states']]
+	predictions_std_over_time_show = np.ones_like(states_next)
+	predictions_std_over_time_show[indexes_control] = \
+		[prediction[num_iter_prediction_ahead_show - 1].numpy()
+			for prediction in prediction_info_over_time['predicted states std']]
+	predictions_over_time_show = \
+		np.concatenate([np.zeros((num_iter_prediction_ahead_show, len(predictions_over_time_show[0]))),
+			predictions_over_time_show[:-num_iter_prediction_ahead_show]])
+	predictions_std_over_time_show = \
+		np.concatenate([np.ones((num_iter_prediction_ahead_show, len(predictions_over_time_show[0]))),
+			predictions_std_over_time_show[:-num_iter_prediction_ahead_show]])
+	mean_cost_traj = np.array([element for element in prediction_info_over_time['mean predicted cost']])
+	mean_cost_traj_std = np.array([element for element in prediction_info_over_time['mean predicted cost std']])
+	np.nan_to_num(predictions_over_time_show, copy=False, nan=-1, posinf=-1, neginf=-1)
+	np.nan_to_num(predictions_std_over_time_show, copy=False, nan=np.max(predictions_std_over_time_show), posinf=99,
+		neginf=0)
+	np.nan_to_num(mean_cost_traj, copy=False, nan=-1, posinf=-1, neginf=-1)
+	np.nan_to_num(predictions_std_over_time_show, copy=False, nan=np.max(mean_cost_traj_std), posinf=99, neginf=0)
+	for state_idx in range(len(states[0])):
+		axes[0].plot(indexes, np.array(states)[:, state_idx], label='state' + str(state_idx),
+			color=plt.get_cmap('tab20').colors[2 * state_idx])
+		axes[0].plot(indexes, predictions_over_time_show[:, state_idx],
+			label=str(num_iter_prediction_ahead_show * num_repeat_actions) + 'step_' + 'prediction' + str(state_idx),
+			color=plt.get_cmap('tab20').colors[2 * state_idx], linestyle='--')
+		axes[0].fill_between(indexes,
+			predictions_over_time_show[:, state_idx] - predictions_std_over_time_show[:, state_idx],
+			predictions_over_time_show[:, state_idx] + predictions_std_over_time_show[:, state_idx],
+			label=str(num_iter_prediction_ahead_show * num_repeat_actions) + 'step_' + 'bounds_prediction' + str(
+				state_idx), color=plt.get_cmap('tab20').colors[2 * state_idx], alpha=ALPHA_CONFIDENCE_BOUNDS)
+		if constraints_states is not None and constraints_states['use_constraints']:
+			axes[0].axhline(y=constraints_states['states_min'][state_idx],
+				color=plt.get_cmap('tab20').colors[2 * state_idx],
+				linestyle='-.')
+			axes[0].axhline(y=constraints_states['states_max'][state_idx],
+				color=plt.get_cmap('tab20').colors[2 * state_idx],
+				linestyle='-.')
+	costs = np.array([element for element in prediction_info_over_time['cost']])
+	axes[2].plot(list(indexes_control * num_repeat_actions), costs, label='cost', color='k')
+	axes[2].plot(list(indexes_control * num_repeat_actions), mean_cost_traj, label='mean predicted cost', color='orange')
+	axes[2].fill_between(list(indexes_control * num_repeat_actions), mean_cost_traj - 2 * mean_cost_traj_std,
+		mean_cost_traj + 3 * mean_cost_traj_std, label='mean predicted cost 3 std confidence bounds', color='orange',
+		alpha=ALPHA_CONFIDENCE_BOUNDS)
+
+	for action_idx in range(len(actions[0])):
+		axes[1].step(indexes, np.array(actions)[:, action_idx], label='a_' + str(action_idx),
+			color=plt.get_cmap('tab20').colors[1 + 2 * action_idx])
+
+	if update_x_limit is True:
+		axes[2].set_xlim(0, np.max(indexes))
+	axes[2].set_ylim(0, np.max([np.max(mean_cost_traj), np.max(costs)]) * 1.2)
+	return fig, axes
 
 
 def save_plot_history_process(states, actions, states_next, prediction_info_over_time, folder_save,
 		num_repeat_actions=1, constraints_states=None, num_iter_prediction_ahead_show=3):
 	with threadpool_limits(limits=1, user_api='blas'), threadpool_limits(limits=1, user_api='openmp'):
-		indexes = np.arange(0, len(states) * num_repeat_actions, num_repeat_actions)
-		fig, axes = plt.subplots(nrows=3, figsize=(12, 10))
+		fig, axes = plt.subplots(nrows=3, figsize=(6, 5), sharex=True)
 		axes[0].set_title('Normed states and predictions')
 		axes[1].set_title('Normed actions')
 		axes[2].set_title('Cost and horizon cost')
-		indexes_control = np.array(prediction_info_over_time['iteration']) // num_repeat_actions
-		predictions_over_time_show = np.zeros_like(states_next)
-		predictions_over_time_show[indexes_control] = \
-			[prediction[num_iter_prediction_ahead_show - 1].numpy()
-				for prediction in prediction_info_over_time['predicted states']]
-		predictions_std_over_time_show = np.ones_like(states_next)
-		predictions_std_over_time_show[indexes_control] = \
-			[prediction[num_iter_prediction_ahead_show - 1].numpy()
-				for prediction in prediction_info_over_time['predicted states std']]
-		predictions_over_time_show = \
-			np.concatenate([np.zeros((num_iter_prediction_ahead_show, len(predictions_over_time_show[0]))),
-												predictions_over_time_show[:-num_iter_prediction_ahead_show]])
-		predictions_std_over_time_show = \
-			np.concatenate([np.ones((num_iter_prediction_ahead_show, len(predictions_over_time_show[0]))),
-				predictions_std_over_time_show[:-num_iter_prediction_ahead_show]])
-		mean_cost_traj = np.array([element for element in prediction_info_over_time['mean cost trajectory']])
-		mean_cost_traj_std = np.array([element for element in prediction_info_over_time['mean cost trajectory std']])
-		np.nan_to_num(predictions_over_time_show, copy=False, nan=-1, posinf=-1, neginf=-1)
-		np.nan_to_num(predictions_std_over_time_show, copy=False, nan=np.max(predictions_std_over_time_show), posinf=99, neginf=0)
-		np.nan_to_num(mean_cost_traj, copy=False, nan=-1, posinf=-1, neginf=-1)
-		np.nan_to_num(predictions_std_over_time_show, copy=False, nan=np.max(mean_cost_traj_std), posinf=99, neginf=0)
-		for state_idx in range(len(states[0])):
-			axes[0].plot(indexes, np.array(states)[:, state_idx], label='state' + str(state_idx),
-				color=plt.get_cmap('tab20').colors[2 * state_idx])
-			axes[0].plot(indexes, predictions_over_time_show[:, state_idx],
-				label=str(num_iter_prediction_ahead_show * num_repeat_actions) + 'step_' + 'prediction' + str(state_idx),
-				color=plt.get_cmap('tab20').colors[2 * state_idx], linestyle='--')
-			axes[0].fill_between(indexes,
-				predictions_over_time_show[:, state_idx] - predictions_std_over_time_show[:, state_idx],
-				predictions_over_time_show[:, state_idx] + predictions_std_over_time_show[:, state_idx],
-				label=str(num_iter_prediction_ahead_show * num_repeat_actions) + 'step_' + 'bounds_prediction' + str(state_idx),
-				color=plt.get_cmap('tab20').colors[2 * state_idx + 1], alpha=0.6)
-			if constraints_states is not None and constraints_states['use_constraints']:
-				axes[0].axhline(y=constraints_states['states_min'][state_idx],
-					color=plt.get_cmap('tab20').colors[2 * state_idx + 1],
-					linestyle='-.')
-				axes[0].axhline(y=constraints_states['states_max'][state_idx],
-					color=plt.get_cmap('tab20').colors[2 * state_idx + 1],
-					linestyle='-.')
-		costs = np.array([element for element in prediction_info_over_time['cost']])
-		axes[2].plot(list(indexes_control * num_repeat_actions), costs, label='cost', color='k')
-		axes[2].plot(list(indexes_control * num_repeat_actions), mean_cost_traj, label='cost trajectory', color='orange')
-		axes[2].fill_between(list(indexes_control * num_repeat_actions), mean_cost_traj - 2 * mean_cost_traj_std,
-			mean_cost_traj + 3 * mean_cost_traj_std, label='cost trajectory 3 std', alpha=0.6)
-
-		for action_idx in range(len(actions[0])):
-			axes[1].step(indexes, np.array(actions)[:, action_idx], label='a_' + str(action_idx))
-
 		plt.xlabel("time")
+		axes[0].set_ylim(0, 1.02)
+		axes[1].set_ylim(0, 1.02)
+		plt.tight_layout()
+		fig, axes = draw_history(states, actions, states_next, prediction_info_over_time, num_repeat_actions,
+		constraints_states, num_iter_prediction_ahead_show, fig, axes)
 		axes[0].legend()
 		axes[1].legend()
 		axes[2].legend()
-		axes[0].set_ylim(0, 1.02)
-		axes[1].set_ylim(0, 1.02)
-		axes[2].set_ylim(0, np.max([np.max(mean_cost_traj), np.max(costs)]) * 1.2)
-		axes[2].set_xlim(0, np.max(indexes))
-		plt.tight_layout()
 		hour = datetime.datetime.now().hour
 		minute = datetime.datetime.now().minute
 		second = datetime.datetime.now().second
 		fig.savefig(os.path.join(folder_save, 'history' + '_h' + str(hour) + '_m' + str(minute)
 											  + '_s' + str(second) + '.png'))
 		plt.close()
+
+
+class LivePlotClass:
+	def __init__(self, num_steps_total, observation_space, action_space, constraints,
+			step_pred, num_std_bounds=2, fontsize=6):
+		self.fig, self.axes = plt.subplots(nrows=3, figsize=(6, 5), sharex=True)
+		self.axes[0].set_title('Normed states and predictions')
+		self.axes[1].set_title('Normed actions')
+		self.axes[2].set_title('Cost and horizon cost')
+		plt.xlabel("Env steps")
+		self.axes[0].set_ylim(-0.03, 1.03)
+		self.axes[1].set_ylim(-0.03, 1.03)
+		self.axes[2].set_xlim(0, num_steps_total)
+		plt.tight_layout()
+
+		num_steps_actions = num_steps_total // step_pred
+		self.step_pred = step_pred
+		self.num_std_bounds = num_std_bounds
+
+		self.states = np.empty((num_steps_actions, observation_space.shape[0]))
+		self.actions = np.empty((num_steps_actions, action_space.shape[0]))
+		self.costs = np.empty(num_steps_actions)
+		self.mean_predicted_cost = np.empty_like(self.costs)
+		self.mean_predicted_cost_std = np.empty_like(self.mean_predicted_cost)
+
+		self.min_observation = observation_space.low
+		self.max_observation = observation_space.high
+		self.min_action = action_space.low
+		self.max_action = action_space.high
+
+		self.num_points_show = 0
+		if constraints is not None and constraints['use_constraints']:
+			for state_idx in range(observation_space.shape[0]):
+				self.axes[0].axhline(y=constraints['states_min'][state_idx],
+					color=plt.get_cmap('tab20').colors[2 * state_idx],
+					linestyle='-.')
+				self.axes[0].axhline(y=constraints['states_max'][state_idx],
+					color=plt.get_cmap('tab20').colors[2 * state_idx],
+					linestyle='-.')
+		self.states_lines = [self.axes[0].plot([], [], label='state' + str(state_idx),
+				color=plt.get_cmap('tab20').colors[2 * state_idx]) for state_idx in range(observation_space.shape[0])]
+		self.actions_lines = [self.axes[1].step([], [], label='action' + str(action_idx),
+				color=plt.get_cmap('tab20').colors[1 + 2 * action_idx]) for action_idx in range(action_space.shape[0])]
+
+		self.cost_line = self.axes[2].plot([], [], label='cost', color='k')
+		self.mean_predicted_cost_line = self.axes[2].plot([], [], label='mean predicted cost', color='orange')
+
+		self.predicted_states_lines = [self.axes[0].plot([], [],
+				label='predicted_states' + str(state_idx),
+				color=plt.get_cmap('tab20').colors[2 * state_idx], linestyle='dashed')
+			for state_idx in range(observation_space.shape[0])]
+		self.predicted_actions_line = [self.axes[1].step([], [], label='predicted_action' + str(action_idx),
+				color=plt.get_cmap('tab20').colors[1 + 2 * action_idx], linestyle='dashed')
+				for action_idx in range(action_space.shape[0])]
+		self.predicted_cost_line = self.axes[2].plot([], [], label='predicted cost', color='k', linestyle='dashed')
+
+		self.axes[0].legend(fontsize=fontsize)
+		self.axes[1].legend(fontsize=fontsize)
+		self.axes[2].legend(fontsize=fontsize)
+		plt.show(block=False)
+
+	def add_point_update(self, observation, action, add_info_dict=None):
+		state = (observation - self.min_observation) / (self.max_observation - self.min_observation)
+		action = (action - self.min_action) / (self.max_action - self.min_action)
+		self.states[self.num_points_show] = state
+		self.actions[self.num_points_show] = action
+		x_indexes = torch.arange(0, (self.num_points_show + 1) * self.step_pred, self.step_pred)
+		for idx_axes in range(len(self.axes)):
+			self.axes[idx_axes].collections.clear()
+
+		for state_idx in range(len(state)):
+			self.states_lines[state_idx][0].set_data(x_indexes, self.states[:(self.num_points_show + 1), state_idx])
+		for action_idx in range(len(action)):
+			self.actions_lines[action_idx][0].set_data(x_indexes, self.actions[:(self.num_points_show + 1), action_idx])
+
+		if add_info_dict is not None:
+			cost = add_info_dict['cost']
+			mean_predicted_cost = add_info_dict['mean predicted cost']
+			mean_predicted_cost_std = add_info_dict['mean predicted cost std']
+			future_predicted_states = add_info_dict['predicted states']
+			future_predicted_states_std = add_info_dict['predicted states std']
+			future_predicted_actions = add_info_dict['predicted actions']
+			future_predicted_costs = add_info_dict['predicted costs']
+			future_predicted_costs_std = add_info_dict['predicted costs std']
+			np.nan_to_num(mean_predicted_cost, copy=False, nan=-1, posinf=-1, neginf=-1)
+			np.nan_to_num(mean_predicted_cost_std, copy=False, nan=99, posinf=99, neginf=99)
+			np.nan_to_num(future_predicted_states, copy=False, nan=-1, posinf=-1, neginf=-1)
+			np.nan_to_num(future_predicted_states_std, copy=False, nan=99, posinf=99, neginf=0)
+			np.nan_to_num(future_predicted_costs, copy=False, nan=-1, posinf=-1, neginf=-1)
+			np.nan_to_num(future_predicted_costs_std, copy=False, nan=99, posinf=99, neginf=0)
+			future_indexes = torch.arange(x_indexes[-1],
+				x_indexes[-1] + self.step_pred + len(future_predicted_states) * self.step_pred, self.step_pred)
+			self.costs[self.num_points_show] = cost
+			self.mean_predicted_cost[self.num_points_show] = mean_predicted_cost
+			self.mean_predicted_cost_std[self.num_points_show] = mean_predicted_cost_std
+
+			for state_idx in range(len(state)):
+				future_states_show = np.concatenate((state[None, state_idx], future_predicted_states[:, state_idx]))
+				self.predicted_states_lines[state_idx][0].set_data(future_indexes, future_states_show)
+				future_states_std_show = np.concatenate(([0], future_predicted_states_std[:, state_idx]))
+				self.axes[0].fill_between(future_indexes,
+					future_states_show - future_states_std_show * self.num_std_bounds,
+					future_states_show + future_states_std_show * self.num_std_bounds,
+					facecolor=plt.get_cmap('tab20').colors[2 * state_idx], alpha=ALPHA_CONFIDENCE_BOUNDS,
+					label='predicted ' + str(self.num_std_bounds) + ' std bounds state ' + str(state_idx))
+			for action_idx in range(len(action)):
+				self.predicted_actions_line[action_idx][0].set_data(future_indexes,
+					np.concatenate((action[None, action_idx], future_predicted_actions[:, action_idx])))
+
+			future_costs_show = np.concatenate(([cost], future_predicted_costs))
+			self.predicted_cost_line[0].set_data(future_indexes, future_costs_show)
+
+			future_cost_std_show = np.concatenate(([0], future_predicted_costs_std))
+			self.axes[2].fill_between(future_indexes,
+				future_costs_show - future_cost_std_show * self.num_std_bounds,
+				future_costs_show + future_cost_std_show * self.num_std_bounds,
+				facecolor='black', alpha=ALPHA_CONFIDENCE_BOUNDS,
+				label='predicted ' + str(self.num_std_bounds) + ' std cost bounds')
+		else:
+			self.costs[self.num_points_show] = 0
+			self.mean_predicted_cost[self.num_points_show] = 0
+			self.mean_predicted_cost_std[self.num_points_show] = 10
+
+		self.cost_line[0].set_data(x_indexes, self.costs[:(self.num_points_show + 1)])
+		self.mean_predicted_cost_line[0].set_data(x_indexes, self.mean_predicted_cost[:(self.num_points_show + 1)])
+		self.axes[2].set_ylim(0, np.max([np.max(self.mean_predicted_cost[:(self.num_points_show + 1)]),
+												np.max(self.costs[:(self.num_points_show + 1)])]) * 1.1)
+		self.axes[2].fill_between(x_indexes,
+			self.mean_predicted_cost[:(self.num_points_show + 1)] -
+			self.mean_predicted_cost_std[:(self.num_points_show + 1)] * self.num_std_bounds,
+			self.mean_predicted_cost[:(self.num_points_show + 1)] +
+			self.mean_predicted_cost_std[:(self.num_points_show + 1)] * self.num_std_bounds,
+			facecolor='orange', alpha=ALPHA_CONFIDENCE_BOUNDS,
+			label='mean predicted ' + str(self.num_std_bounds) + ' std cost bounds')
+
+		self.fig.canvas.draw()
+		self.num_points_show += 1
 
 
 def save_plot_model_3d_process(input_data, output_data, parameters, constraints, folder_save,

main.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 import gym
 import json
 from gym.wrappers.monitoring.video_recorder import VideoRecorder
+import torch
 
 from control_objects.probabilistic_gp_mpc_controller import ProbabiliticGpMpcController
+from control_objects.utils import draw_history, LivePlotClass
 
 
 def main():
@@ -35,10 +37,19 @@ def main():
 	num_repeat_actions = params_controller["num_repeat_actions"]
 	num_tests = params_general['number_tests_to_run']
 
+	target_state = np.array(params_controller['target_state'])
+	weights_target_state = np.diag(params_controller['weights_target_state'])
+	weights_target_state_terminal_cost = np.diag(params_controller['weights_target_state_terminal_cost'])
+	target_action = np.array(params_controller['target_action'])
+	weights_target_action = np.diag(params_controller['weights_target_action'])
+	s_observation = np.diag(params_controller['s_observation'])
+
 	losses_tests = np.ones((num_tests, num_steps // num_repeat_actions - num_random_actions))
 	for test_idx in range(num_tests):
 		env = gym.make(env_to_control)
-
+		if params_general['render_live_plot']:
+			live_plot_obj = LivePlotClass(num_steps,
+				env.observation_space, env.action_space, params_constraints_states, num_repeat_actions)
 		datetime_now = datetime.datetime.now()
 		folder_save = os.path.join('folder_save', env_to_control, 'y' + str(datetime_now.year) \
 																  + '_mon' + str(datetime_now.month) + '_d' + str(
@@ -55,20 +66,13 @@ def main():
 
 		env.reset()
 
-		target = np.array(params_controller['target'])
-		weights_target = np.diag(params_controller['weights_target'])
-		weights_target_terminal_cost = np.diag(params_controller['weights_target_terminal_cost'])
-		s_observation = np.diag(params_controller['s_observation'])
-
 		control_object = ProbabiliticGpMpcController(env.observation_space, env.action_space, params_controller,
-			params_train, params_actions_optimizer, params_constraints_states,
-			hyperparameters_init, target, weights_target, weights_target_terminal_cost, params_constraints_hyperparams,
-			env_to_control,
-			folder_save, num_repeat_actions)
+			params_train, params_actions_optimizer, params_constraints_states, hyperparameters_init,
+			target_state, weights_target_state, weights_target_state_terminal_cost,
+			target_action, weights_target_action,
+			params_constraints_hyperparams, env_to_control, folder_save, num_repeat_actions)
 
-		action = env.action_space.sample()
-		# low + (env.action_space.high - env.action_space.low) * np.random.uniform(0, 1)
-		observation, reward, done, info = env.step(action)
+		observation, reward, done, info = env.step(env.action_space.sample())
 		for idx_random_action in range(num_random_actions):
 			control_object.action = env.action_space.sample()
 			action = env.action_space.sample()
@@ -84,6 +88,8 @@ def main():
 						rec.capture_frame()
 					except:
 						pass
+			if params_general['render_live_plot']:
+				live_plot_obj.add_point_update(observation, action)
 			control_object.add_point_memory(observation, action, new_observation, reward)
 			observation = new_observation
 
@@ -108,9 +114,8 @@ def main():
 			if params_general['verbose']:
 				for key in add_info_dict:
 					print(key + ': ' + str(add_info_dict[key]))
-			observation = new_observation
 
-			if params_general['save_plot_history'] and \
+			if params_general['save_plot'] and \
 					(control_object.num_points_memory % params_general["frequency_iter_save"] == 0):
 				control_object.save_plot_history()
 
@@ -118,13 +123,35 @@ def main():
 					(control_object.num_points_memory % params_general["frequency_iter_save"] == 0):
 				control_object.save_plot_model_3d()
 
-			# TODO: Allow dynamic visualizations
-			if params_general['render_live_plot_model_3d']:
-				pass  # control_object.render_plot_model3d()
+			if params_general['render_live_plot']:
+				live_plot_obj.add_point_update(observation, action, add_info_dict)
+				'''if not 'fig_history' in vars() or not 'axes_history' in vars():
+					fig_history, axes_history = plt.subplots(nrows=3, figsize=(6, 5), sharex=True)
+					axes_history[0].set_title('Normed states and predictions')
+					axes_history[1].set_title('Normed actions')
+					axes_history[2].set_title('Cost and horizon cost')
+					plt.xlabel("time")
+					axes_history[0].set_ylim(0, 1.02)
+					axes_history[1].set_ylim(0, 1.02)
+					axes_history[2].set_xlim(0, num_steps)
+					plt.tight_layout()
+					states = control_object.x[:control_object.num_points_memory, :control_object.num_states].numpy()
+					actions = control_object.x[:control_object.num_points_memory, control_object.num_states:].numpy()
+					states_next = control_object.y[:control_object.num_points_memory].numpy() + states
+					fig_history, axes_history = draw_history(states, actions, states_next,
+						control_object.prediction_info_over_time, num_repeat_actions, control_object.constraints_states,
+						3, fig_history, axes_history, False)
+					plt.show(block=False)
+				else:
+					states = control_object.x[:control_object.num_points_memory, :control_object.num_states].numpy()
+					actions = control_object.x[:control_object.num_points_memory, control_object.num_states:].numpy()
+					states_next = control_object.y[:control_object.num_points_memory].numpy() + states
+					fig_history, axes_history = draw_history(states, actions, states_next,
+						control_object.prediction_info_over_time, num_repeat_actions, control_object.constraints_states,
+						3, fig_history, axes_history, False)
+					fig_history.canvas.draw()'''
 
-			if params_general['render_live_plot_history']:
-				pass  # control_object.render_plot_model3d()
-
+			observation = new_observation
 			print("time loop: " + str(time.time() - time_start) + " s\n")
 		env.__exit__()
 		if params_general['save_render_env']:
@@ -137,6 +164,7 @@ def main():
 		# wait for the processes to be finished
 		control_object.p_save_plot_model_3d.join()
 		control_object.p_save_plot_model_3d.close()
+
 		control_object.p_save_plot_history.join()
 		control_object.p_save_plot_history.close()
 